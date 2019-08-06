68418aabaf25a3b1293023500634e7bda5988e2a
Travis Exline <t.exline22@gmail.com>
2019-08-06 15:31:00 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,5 @@ class SessionsController < ApplicationController
     return redirect_to(controller: 'sessions', action: 'new') unless user
     session[:user_id] = user.id
     @user = user
-    redirect_to controller: 'welcome', action: 'home'
   end
 end
